2b320ba0895a265514787228cbf9c989a2b740bd
Thonatos <sven4369@gmail.com>
2023-05-08 16:47:12 +0300

    markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  userNameId = nanoid();
+  userNumberId = nanoid();
+
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor={this.userNameId}>
+            Name
+            <input type="text" id={this.userNameId} />
+          </label>
+
+          <label htmlFor={this.userNumberId}>
+            Number
+            <input
+              type="tel"
+              name="number"
+              id={this.userNameId}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+
+        <span>Contacts</span>
+        <ul>
+
+        </ul>
+      </div>
+    );
+  }
+}
+
+export { App };
